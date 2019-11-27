11c6f9fc45140a37f9cf1148778bef48c6f45f85
ksxue <ksxue@users.noreply.github.com>
2019-11-26 17:01:05 -0800

    Add data, add tidy data format

--- PitcherPlant.R
+++ PitcherPlant.R
@@ -1,6 +1,15 @@
-install.packages("tidyverse")
 library(tidyverse)
 
+data <- read.table("otu_table_16S_obs10_bittleston.txt",
+                   header=TRUE, stringsAsFactors = FALSE, sep="\t")
+
+# otu | taxonomy | sample | count
+tidydata <- data %>%
+  gather(key="sample", value="count",X26Gt.48:X10Nr.21)
+
+# tidyverse:
+# https://www.analyticsvidhya.com/blog/2019/05/beginner-guide-tidyverse-most-powerful-collection-r-packages-data-science/
+# creating tidy data: https://rstudio-pubs-static.s3.amazonaws.com/282405_e280f5f0073544d7be417cde893d78d0.html 
 
 ##observed_species_16s_6500.txt dataset
 path <- file.path("~", "desktop", "observed_species_16s_6500.txt")
