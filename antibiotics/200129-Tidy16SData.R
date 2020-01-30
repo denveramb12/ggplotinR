be4acdc7cd11a5517ef2dc8c98676238fedc2830
ksxue <ksxue@users.noreply.github.com>
2020-01-29 17:30:29 -0800

    Add starting tasks for getting familiar with phyloseq data.

--- antibiotics/200129-Tidy16SData.R
+++ antibiotics/200129-Tidy16SData.R
@@ -9,6 +9,18 @@ pilotsamples <- (sample_data(data) %>%
                    filter(Subject %in% c("DBU","DBV","EAQ","EBF")))$Meas_ID
 # Retain only samples belonging to the four pilot subjects.
 data <- prune_samples(pilotsamples, data)
+
 # Prune OTUs that are absent from the four pilot subjects.
+# Use prune_taxa.
+# How many taxa are remaining?
 
 
+# Plot a timeline of samples for each of the four pilot subjects,
+# relative to antibiotic treament. (Abx_RelDay)
+
+
+# What was the average weight of the pilot subjects? (Weight)
+
+
+# Plot the number of samples for each subject analyzed on different
+# sequencing runs. (Sequencing_Run)
