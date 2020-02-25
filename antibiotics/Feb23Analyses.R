3ba5bcd8f5606fcd44c242e64f7f3ddd03e420de
denveramb12 <56239793+denveramb12@users.noreply.github.com>
2020-02-24 19:00:47 -0800

    Update Feb23Analyses.R

--- antibiotics/Feb23Analyses.R
+++ antibiotics/Feb23Analyses.R
@@ -1,8 +1,25 @@
 library(phyloseq)
 library(tidyverse)
 
+
 data <- readRDS("ps0995_4alpha_29Sep2019.rds")
+pilotsamples <- (sample_data(data) %>%
+                   filter(Subject %in% c("DBU","DBV","EAQ","EBF")))$Meas_ID
+
+data <- prune_samples(pilotsamples, data)
+data <- prune_taxa(taxa_sums(data) > 0,data)
 gpt <- subset_taxa(data, Kingdom=="Bacteria")
-gpt <- prune_taxa(names(sort(taxa_sums(data),TRUE)[1:300]), data)
-plot_heatmap(gpt)
+gpt <- prune_taxa(names(sort(taxa_sums(data),TRUE)[1:331]), data)
+p <- plot_heatmap(gpt, "Genus") + facet_wrap(~Subject)
+p
+
+##still working on finding how to track number of ref OTU on any
+##given abx_relDay
+sample_data(gpt)%>% ggplot() + geom_line(aes(x = Abx_RelDay, y =)))+
+  facet_wrap(~Subject)
+
+
+
+
+
 
