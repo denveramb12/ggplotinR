7350b4018e4525f6977261eb2f51dcdea9dc5866
denveramb12 <56239793+denveramb12@users.noreply.github.com>
2020-03-03 16:08:21 -0800

    Update March1Analyses.R

--- antibiotics/March1Analyses.R
+++ antibiotics/March1Analyses.R
@@ -1,29 +1,38 @@
 library(phyloseq)
 library(tidyverse)
 
-
-data <- readRDS("ps0995_4alpha_29Sep2019.rds")
+data <- readRDS("/Users/arunbalaji/Documents/GitHub/ggplotinR/antibiotics/ps0995_4alpha_29Sep2019.rds")
 pilotsamples <- (sample_data(data) %>%
                    filter(Subject %in% c("DBU","DBV","EAQ","EBF")))$Meas_ID
+dataprune <- prune_samples(pilotsamples, data)
+dataprune <- prune_taxa(taxa_sums(dataprune) > 0,dataprune)
+gpt <- subset_taxa(dataprune, Kingdom=="Bacteria")
+gptconfined <- prune_taxa(names(sort(taxa_sums(dataprune),TRUE)[1:331]), gpt)
 
 
-data <- prune_samples(pilotsamples, data)
-data <- prune_taxa(taxa_sums(data) > 0,data)
-gpt <- subset_taxa(data, Kingdom=="Bacteria")
+pilotsamples2 <- pilotsamples <- (sample_data(data) %>%
+                                    filter(Subject %in% c("CAN","CAC","BSM","BAN")))$Meas_ID
+data2 <- prune_samples(pilotsamples2, data)
+data2 <- prune_taxa(taxa_sums(data2)>0,data2)
+
+gpt2 <- subset_taxa(data2, Kingdom == "Bacteria")
+gpt2confined <- prune_taxa(names(sort(taxa_sums(data2),TRUE)[1:331]), gpt2)
+
+plot_heatmap(gptconfined) + facet_wrap(~Subject)
+plot_heatmap(gpt2confined)+facet_wrap(~Subject)
+
 gpt.ord <- ordinate(gpt, "PCoA")
 ntaxa(gpt)
 
-p1 <- plot_ordination(gpt, gpt.ord, type = "sites",color = "Subject", title ="Samples PCoA")
-p1
+plot_ordination(gpt, gpt.ord, type = "sites",color = "Subject", title ="Samples PCoA")
 
-p <- plot_heatmap(gpt, "Family") + facet_wrap(~Subject)
-p
-
-# do heatmaps with more subjects
-##still working on finding how to track number of ref OTU on any
-##given abx_relDay
-sample_data(gpt)%>% ggplot() + geom_line(aes(x = Abx_RelDay, y =)))+
+##still working on
+sample_data(gpt)%>% ggplot() + geom_line(aes(x = Abx_RelDay, y =))+
   facet_wrap(~Subject)
+
+plot_heatmap(gpt, "Family") + facet_wrap(~Subject)
+
+
 
 
 
